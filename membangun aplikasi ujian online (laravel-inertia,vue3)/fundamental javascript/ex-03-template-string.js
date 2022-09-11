b333ea75c8e87673419289affe101bcb730a37e7
septe habudin <septe.vhirgo@gmail.com>
2022-09-11 10:03:25 +0700

    template string

---
+++ membangun aplikasi ujian online (laravel-inertia,vue3)/fundamental javascript/ex-03-template-string.js
@@ -0,0 +1,10 @@
+// cara lama
+const firstName = "septe";
+const umur = 24;
+console.log("nama depan saya adalah " + firstName + " dan umur saya: " + umur);
+
+// cara modern
+const lastName = "habudin";
+console.log(
+  `nama belakang saya adalah ${lastName} dan umur saya adalah ${umur}`
+);
