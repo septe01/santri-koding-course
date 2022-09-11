cdcdc59b17daba9b621d86673856ae7126a5de2b
septe habudin <septe.vhirgo@gmail.com>
2022-09-11 10:22:20 +0700

    arrow function

---
+++ membangun aplikasi ujian online (laravel-inertia,vue3)/fundamental javascript/ex-arrow-function.js
@@ -0,0 +1,24 @@
+// function biasa
+const nama = function name(nama) {
+  return `nama anda adalah ${nama}`;
+};
+console.log(nama("septe"));
+
+// arrow function
+
+const name = (name) => `nama anda adalah ${name}`;
+
+console.log(name("septe"));
+
+//define function
+const hello = (name, umur) => `Hello ${name} umur ${umur}`;
+
+//panggil function
+console.log(hello("Septehabudin", 25));
+
+const returnObj = (name, umur) => ({
+  status: "OK",
+  mesaage: `Hello ${name} umur ${umur}`,
+});
+
+console.log(returnObj("septe", 32));
